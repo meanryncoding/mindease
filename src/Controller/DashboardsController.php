b935325e4a03962ef52135bd15ec51252538bffc
Asyraf <asyraf.wahianuar@gmail.com>
2024-06-25 21:34:51 +0800

    update count

--- src/Controller/DashboardsController.php
+++ src/Controller/DashboardsController.php
@@ -14,15 +14,29 @@ class DashboardsController extends AppController
 
 		$users = $this->fetchTable('Users');
 		$total_user = $users->find()->all()->count();
+		$active_user = $users->find()->where(['status' => 1])->count();
+		$user_percent = $active_user * 100 / $total_user;
 
 		$contacts = $this->fetchTable('Contacts');
 		$total_contact = $contacts->find()->all()->count();
+		$pending_contact = $contacts->find()->where(['status' => 0])->count();
+		if ($pending_contact == 0) {
+			$pending_contact_percent = 0;
+		} else
+			$pending_contact_percent = $pending_contact * 100 / $total_contact;
 
 		$auditLogs = $this->fetchTable('auditLogs');
 		$total_auditlog = $auditLogs->find()->all()->count();
 
 		$todos = $this->fetchTable('Todos');
 		$total_todo = $todos->find()->all()->count();
+		$pending_todo = $todos->find()->where(['status' => 'Pending'])->count();
+		$pending_todo_percent = $pending_todo * 100 / $total_todo;
+
+		$faqs = $this->fetchTable('Faqs');
+		$total_faq = $faqs->find()->all()->count();
+		$pending_faq = $faqs->find()->where(['status' => 1])->count();
+		$pending_faq_percent = $pending_faq * 100 / $total_faq;
 
 		/* $userLogs = $this->fetchTable('userLogs');
 		$userLogs = $this->userLogs->find('all')
@@ -33,6 +47,6 @@ class DashboardsController extends AppController
 		//debug($activity);
 		//exit;
 
-		$this->set(compact('total_user', 'total_contact', 'total_auditlog', 'total_todo'));
+		$this->set(compact('total_user', 'total_contact', 'total_auditlog', 'total_todo', 'user_percent', 'pending_todo_percent', 'pending_faq_percent', 'pending_contact_percent'));
 	}
 }
