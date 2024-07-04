ba074d6c054b27cc2b5456afb0a6c4e3c6f603bd
Asyraf <asyraf.wahianuar@gmail.com>
2024-07-04 08:19:39 +0800

    details update

--- templates/element/offcanvas.php
+++ templates/element/offcanvas.php
@@ -19,11 +19,11 @@
     </div>
     <div class="offcanvas-body px-0">
         <div class="card bg-gold">
-            <div class="p-3">
+            <div class="p-3 text-center">
                 <?php if ($this->Identity->get('avatar') != NULL) {
-                    echo $this->Html->image('../files/Users/avatar/' . $this->Identity->get('slug') . '/' . $this->Identity->get('avatar'), ['class' => 'd-block rounded shadow', 'width' => '130px', 'height' => '130px']);
+                    echo $this->Html->image('../files/Users/avatar/' . $this->Identity->get('slug') . '/' . $this->Identity->get('avatar'), ['class' => 'd-block rounded-circle shadow', 'width' => '130px', 'height' => '130px']);
                 } else
-                    echo $this->Html->image('blank_profile.png', ['class' => 'd-block rounded shadow', 'width' => '130px', 'height' => '130px']);
+                    echo $this->Html->image('blank_profile.png', ['class' => 'd-block rounded-circle shadow', 'width' => '130px', 'height' => '130px']);
                 ?>
             </div>
         </div>
@@ -33,65 +33,70 @@
             <table class="table table-borderless table-sm">
                 <tr>
                     <th width="20%">Name</th>
-                    <td><?php //echo $this->Identity->get('fullname'); 
+                    <td><?php echo $this->Identity->get('fullname');
                         ?></td>
                 </tr>
                 <tr>
                     <th>Email</th>
-                    <td><?php //echo $this->Identity->get('email'); 
+                    <td><?php echo $this->Identity->get('email');
                         ?></td>
                 </tr>
                 <tr>
                     <th>Last Login</th>
-                    <td><?php //echo date('M d, Y (h:i A)', strtotime($this->Identity->get('last_login'))); 
+                    <td><?php echo date('M d, Y (h:i A)', strtotime($this->Identity->get('last_login')));
                         ?></td>
                 </tr>
                 <tr>
                     <th>Group</th>
                     <td>
-                        <?php /* if ($this->Identity->get('user_group_id') == 1){
-	echo 'Administrator';
-}elseif ($this->Identity->get('user_group_id') == 2){
-	echo 'Moderator';
-}elseif ($this->Identity->get('user_group_id') == 3){
-	echo 'User';
-}else
-	echo 'Error'; */
+                        <?php if ($this->Identity->get('user_group_id') == 1) {
+                            echo 'Administrator';
+                        } elseif ($this->Identity->get('user_group_id') == 2) {
+                            echo 'Moderator';
+                        } elseif ($this->Identity->get('user_group_id') == 3) {
+                            echo 'User';
+                        } else
+                            echo 'Error';
                         ?>
                     </td>
                 </tr>
                 <tr>
                     <th>Status</th>
                     <td>
-                        <?php /* if ($this->Identity->get('status') == 1){
-				echo '<span class="badge bg-success">Active</span>';
-			}elseif ($this->Identity->get('status') == 0){
-				echo '<span class="badge bg-danger">Disabled</span>';
-			}else
-				echo '<span class="badge bg-secondary">Archived</span>'; */
+                        <?php if ($this->Identity->get('status') == 1) {
+                            echo '<span class="badge bg-success">Active</span>';
+                        } elseif ($this->Identity->get('status') == 0) {
+                            echo '<span class="badge bg-danger">Disabled</span>';
+                        } else
+                            echo '<span class="badge bg-secondary">Archived</span>';
                         ?>
                     </td>
                 </tr>
                 <tr>
                     <th>Verified</th>
                     <td>
-                        <?php /* if ($this->Identity->get('is_email_verified') == 1){
-	echo '<span class="badge bg-success">Verified</span>';
-}else
-	echo '<span class="badge bg-danger">Not verified</span>'; */
+                        <?php if ($this->Identity->get('is_email_verified') == 1) {
+                            echo '<span class="badge bg-success">Verified</span>';
+                        } else
+                            echo '<span class="badge bg-danger">Not verified</span>';
                         ?>
                     </td>
                 </tr>
                 <tr>
                     <th>Created on</th>
-                    <td><?php //echo date('M d, Y (h:i A)', strtotime($this->Identity->get('created'))); 
+                    <td><?php echo date('M d, Y (h:i A)', strtotime($this->Identity->get('created')));
                         ?></td>
                 </tr>
             </table>
 
-            <div class="text-end">
-                xxx
-            </div>
+            <?php if ($this->Identity->isLoggedIn()) {
+            ?>
+                <div class="text-end">
+                    <?php echo $this->Html->link('<i class="fa-solid fa-arrow-right-from-bracket"></i> Logout', ['controller' => 'Users', 'action' => 'logout', 'prefix' => false], ['class' => 'btn btn-sm btn-outline-primary transparent', 'escape' => false, 'alt' => 'Sign-out']); ?>
+
+                </div>
+            <?php }
+            ?>
 
 
 
