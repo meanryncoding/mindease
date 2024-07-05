8f837958345875665fe6ce9ffd7f2e5d0d612100
Asyraf <asyraf.wahianuar@gmail.com>
2024-07-05 11:13:18 +0800

    update count query

--- src/Controller/BooksController.php
+++ src/Controller/BooksController.php
@@ -1,11 +1,7 @@
 <?php
-
 declare(strict_types=1);
 
 namespace App\Controller;
-
-use Cake\Routing\Router;
-use Cake\Event\EventManager;
 
 /**
  * Books Controller
@@ -14,58 +10,58 @@ use Cake\Event\EventManager;
  */
 class BooksController extends AppController
 {
-    public function initialize(): void
-    {
-        parent::initialize();
+	public function initialize(): void
+	{
+		parent::initialize();
 
-        $this->loadComponent('Search.Search', [
-            'actions' => ['index'],
-        ]);
-    }
+		$this->loadComponent('Search.Search', [
+			'actions' => ['index'],
+		]);
+	}
+	
+	public function beforeFilter(\Cake\Event\EventInterface $event)
+	{
+		parent::beforeFilter($event);
+	}
 
-    public function beforeFilter(\Cake\Event\EventInterface $event)
-    {
-        parent::beforeFilter($event);
-    }
-
-    /*public function viewClasses(): array
+	/*public function viewClasses(): array
     {
         return [JsonView::class];
 		return [JsonView::class, XmlView::class];
     }*/
-
-    public function json()
+	
+	public function json()
     {
-        $this->viewBuilder()->setLayout('json');
+		$this->viewBuilder()->setLayout('json');
         $this->set('books', $this->paginate());
         $this->viewBuilder()->setOption('serialize', 'books');
     }
+	
+	public function csv()
+	{
+		$this->response = $this->response->withDownload('books.csv');
+		$books = $this->Books->find();
+		$_serialize = 'books';
 
-    public function csv()
-    {
-        $this->response = $this->response->withDownload('books.csv');
-        $books = $this->Books->find();
-        $_serialize = 'books';
-
-        $this->viewBuilder()->setClassName('CsvView.Csv');
-        $this->set(compact('books', '_serialize'));
-    }
-
-    public function pdfList()
-    {
-        $this->viewBuilder()->enableAutoLayout(false);
-        $books = $this->paginate($this->Books);
-        $this->viewBuilder()->setClassName('CakePdf.Pdf');
-        $this->viewBuilder()->setOption(
-            'pdfConfig',
-            [
-                'orientation' => 'portrait',
-                'download' => true,
-                'filename' => 'books_List.pdf'
-            ]
-        );
-        $this->set(compact('books'));
-    }
+		$this->viewBuilder()->setClassName('CsvView.Csv');
+		$this->set(compact('books', '_serialize'));
+	}
+	
+	public function pdfList()
+	{
+		$this->viewBuilder()->enableAutoLayout(false); 
+		$books = $this->paginate($this->Books);
+		$this->viewBuilder()->setClassName('CakePdf.Pdf');
+		$this->viewBuilder()->setOption(
+			'pdfConfig',
+			[
+				'orientation' => 'portrait',
+				'download' => true, 
+				'filename' => 'books_List.pdf' 
+			]
+		);
+		$this->set(compact('books'));
+	}
     /**
      * Index method
      *
@@ -73,58 +69,90 @@ class BooksController extends AppController
      */
     public function index()
     {
-        $this->set('title', 'Books List');
-        $this->paginate = [
-            'maxLimit' => 10,
+		$this->set('title', 'Books List');
+		$this->paginate = [
+			'maxLimit' => 10,
         ];
         $query = $this->Books->find('search', search: $this->request->getQueryParams());
         $books = $this->paginate($query);
+		
+		//count
+		$this->set('total_books', $this->Books->find()->count());
+		$this->set('total_books_archived', $this->Books->find()->where(['status' => 2])->count());
+		$this->set('total_books_active', $this->Books->find()->where(['status' => 1])->count());
+		$this->set('total_books_disabled', $this->Books->find()->where(['status' => 0])->count());
+		
+		//Count By Month
+		$this->set('january', $this->Books->find()->where(['MONTH(created)' => date('1'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('february', $this->Books->find()->where(['MONTH(created)' => date('2'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('march', $this->Books->find()->where(['MONTH(created)' => date('3'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('april', $this->Books->find()->where(['MONTH(created)' => date('4'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('may', $this->Books->find()->where(['MONTH(created)' => date('5'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('jun', $this->Books->find()->where(['MONTH(created)' => date('6'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('july', $this->Books->find()->where(['MONTH(created)' => date('7'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('august', $this->Books->find()->where(['MONTH(created)' => date('8'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('september', $this->Books->find()->where(['MONTH(created)' => date('9'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('october', $this->Books->find()->where(['MONTH(created)' => date('10'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('november', $this->Books->find()->where(['MONTH(created)' => date('11'), 'YEAR(created)' => date('Y')])->count());
+		$this->set('december', $this->Books->find()->where(['MONTH(created)' => date('12'), 'YEAR(created)' => date('Y')])->count());
 
-        //count
-        $this->set('total_books', $this->Books->find()->count());
-        $this->set('total_books_archived', $this->Books->find()->where(['status' => 2])->count());
-        $this->set('total_books_active', $this->Books->find()->where(['status' => 1])->count());
-        $this->set('total_books_disabled', $this->Books->find()->where(['status' => 0])->count());
+		$query = $this->Books->find();
 
-        //Count By Month
-        $this->set('january', $this->Books->find()->where(['MONTH(created)' => date('1'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('february', $this->Books->find()->where(['MONTH(created)' => date('2'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('march', $this->Books->find()->where(['MONTH(created)' => date('3'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('april', $this->Books->find()->where(['MONTH(created)' => date('4'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('may', $this->Books->find()->where(['MONTH(created)' => date('5'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('jun', $this->Books->find()->where(['MONTH(created)' => date('6'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('july', $this->Books->find()->where(['MONTH(created)' => date('7'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('august', $this->Books->find()->where(['MONTH(created)' => date('8'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('september', $this->Books->find()->where(['MONTH(created)' => date('9'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('october', $this->Books->find()->where(['MONTH(created)' => date('10'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('november', $this->Books->find()->where(['MONTH(created)' => date('11'), 'YEAR(created)' => date('Y')])->count());
-        $this->set('december', $this->Books->find()->where(['MONTH(created)' => date('12'), 'YEAR(created)' => date('Y')])->count());
+        $expectedMonths = [];
+        for ($i = 11; $i >= 0; $i--) {
+            $expectedMonths[] = date('M-Y', strtotime("-$i months"));
+        }
 
-        //count all user activities and group by date for heatmap
-        //$Books = TableRegistry::getTableLocator()->get('UserLogs');
-        $query = $this->Books->find();
         $query->select([
             'count' => $query->func()->count('*'),
-            'date' => $query->func()->date_format(['created' => 'identifier', "%m"])
+            'date' => $query->func()->date_format(['created' => 'identifier', "%b-%Y"]),
+            'month' => 'MONTH(created)',
+            'year' => 'YEAR(created)'
         ])
-            ->groupBy(['month' => 'MONTH(created)']);
+            ->where([
+                'created >=' => date('Y-m-01', strtotime('-11 months')),
+                'created <=' => date('Y-m-t')
+            ])
+            ->groupBy(['year', 'month'])
+            ->orderBy(['year' => 'ASC', 'month' => 'ASC']);
 
         $results = $query->all()->toArray();
 
-        $totalBookByMonth = [];
-        foreach ($results as $result) {
-            $totalBookByMonth[] = [
-                'month' => $result->date,
-                'count' => $result->count
+        $totalByMonth = [];
+        foreach ($expectedMonths as $expectedMonth) {
+            $found = false;
+            $count = 0;
+
+            foreach ($results as $result) {
+                if ($expectedMonth === $result->date) {
+                    $found = true;
+                    $count = $result->count;
+                    break;
+                }
+            }
+
+            $totalByMonth[] = [
+                'month' => $expectedMonth,
+                'count' => $count
             ];
         }
 
         $this->set([
-            'results' => $totalBookByMonth,
+            'results' => $totalByMonth,
             '_serialize' => ['results']
         ]);
 
-        $this->set(compact('books', 'totalBookByMonth'));
+        //data as JSON arrays for report chart
+        $totalByMonth = json_encode($totalByMonth);
+        $dataArray = json_decode($totalByMonth, true);
+        $monthArray = [];
+        $countArray = [];
+        foreach ($dataArray as $data) {
+            $monthArray[] = $data['month'];
+            $countArray[] = $data['count'];
+        }
+
+        $this->set(compact('books', 'monthArray', 'countArray'));
     }
 
     /**
@@ -136,7 +164,7 @@ class BooksController extends AppController
      */
     public function view($id = null)
     {
-        $this->set('title', 'Books Details');
+		$this->set('title', 'Books Details');
         $book = $this->Books->get($id, contain: []);
         $this->set(compact('book'));
 
@@ -150,16 +178,16 @@ class BooksController extends AppController
      */
     public function add()
     {
-        $this->set('title', 'New Books');
-        EventManager::instance()->on('AuditStash.beforeLog', function ($event, array $logs) {
-            foreach ($logs as $log) {
-                $log->setMetaInfo($log->getMetaInfo() + ['a_name' => 'Add']);
-                $log->setMetaInfo($log->getMetaInfo() + ['c_name' => 'Books']);
-                $log->setMetaInfo($log->getMetaInfo() + ['ip' => $this->request->clientIp()]);
-                $log->setMetaInfo($log->getMetaInfo() + ['url' => Router::url(null, true)]);
-                $log->setMetaInfo($log->getMetaInfo() + ['slug' => $this->Authentication->getIdentity('slug')->getIdentifier('slug')]);
-            }
-        });
+		$this->set('title', 'New Books');
+		/*EventManager::instance()->on('AuditStash.beforeLog', function ($event, array $logs) {
+			foreach ($logs as $log) {
+				$log->setMetaInfo($log->getMetaInfo() + ['a_name' => 'Add']);
+				$log->setMetaInfo($log->getMetaInfo() + ['c_name' => 'Books']);
+				$log->setMetaInfo($log->getMetaInfo() + ['ip' => $this->request->clientIp()]);
+				$log->setMetaInfo($log->getMetaInfo() + ['url' => Router::url(null, true)]);
+				$log->setMetaInfo($log->getMetaInfo() + ['slug' => $this->Authentication->getIdentity('slug')->getIdentifier('slug')]);
+			}
+		});*/
         $book = $this->Books->newEmptyEntity();
         if ($this->request->is('post')) {
             $book = $this->Books->patchEntity($book, $this->request->getData());
@@ -182,16 +210,16 @@ class BooksController extends AppController
      */
     public function edit($id = null)
     {
-        $this->set('title', 'Books Edit');
-        EventManager::instance()->on('AuditStash.beforeLog', function ($event, array $logs) {
-            foreach ($logs as $log) {
-                $log->setMetaInfo($log->getMetaInfo() + ['a_name' => 'Edit']);
-                $log->setMetaInfo($log->getMetaInfo() + ['c_name' => 'Books']);
-                $log->setMetaInfo($log->getMetaInfo() + ['ip' => $this->request->clientIp()]);
-                $log->setMetaInfo($log->getMetaInfo() + ['url' => Router::url(null, true)]);
-                $log->setMetaInfo($log->getMetaInfo() + ['slug' => $this->Authentication->getIdentity('slug')->getIdentifier('slug')]);
-            }
-        });
+		$this->set('title', 'Books Edit');
+		/*EventManager::instance()->on('AuditStash.beforeLog', function ($event, array $logs) {
+			foreach ($logs as $log) {
+				$log->setMetaInfo($log->getMetaInfo() + ['a_name' => 'Edit']);
+				$log->setMetaInfo($log->getMetaInfo() + ['c_name' => 'Books']);
+				$log->setMetaInfo($log->getMetaInfo() + ['ip' => $this->request->clientIp()]);
+				$log->setMetaInfo($log->getMetaInfo() + ['url' => Router::url(null, true)]);
+				$log->setMetaInfo($log->getMetaInfo() + ['slug' => $this->Authentication->getIdentity('slug')->getIdentifier('slug')]);
+			}
+		});*/
         $book = $this->Books->get($id, [
             'contain' => [],
         ]);
@@ -216,15 +244,15 @@ class BooksController extends AppController
      */
     public function delete($id = null)
     {
-        EventManager::instance()->on('AuditStash.beforeLog', function ($event, array $logs) {
-            foreach ($logs as $log) {
-                $log->setMetaInfo($log->getMetaInfo() + ['a_name' => 'Delete']);
-                $log->setMetaInfo($log->getMetaInfo() + ['c_name' => 'Books']);
-                $log->setMetaInfo($log->getMetaInfo() + ['ip' => $this->request->clientIp()]);
-                $log->setMetaInfo($log->getMetaInfo() + ['url' => Router::url(null, true)]);
-                $log->setMetaInfo($log->getMetaInfo() + ['slug' => $this->Authentication->getIdentity('slug')->getIdentifier('slug')]);
-            }
-        });
+		EventManager::instance()->on('AuditStash.beforeLog', function ($event, array $logs) {
+			foreach ($logs as $log) {
+				$log->setMetaInfo($log->getMetaInfo() + ['a_name' => 'Delete']);
+				$log->setMetaInfo($log->getMetaInfo() + ['c_name' => 'Books']);
+				$log->setMetaInfo($log->getMetaInfo() + ['ip' => $this->request->clientIp()]);
+				$log->setMetaInfo($log->getMetaInfo() + ['url' => Router::url(null, true)]);
+				$log->setMetaInfo($log->getMetaInfo() + ['slug' => $this->Authentication->getIdentity('slug')->getIdentifier('slug')]);
+			}
+		});
         $this->request->allowMethod(['post', 'delete']);
         $book = $this->Books->get($id);
         if ($this->Books->delete($book)) {
@@ -235,29 +263,29 @@ class BooksController extends AppController
 
         return $this->redirect(['action' => 'index']);
     }
-
-    public function archived($id = null)
+	
+	public function archived($id = null)
     {
-        $this->set('title', 'Books Edit');
-        EventManager::instance()->on('AuditStash.beforeLog', function ($event, array $logs) {
-            foreach ($logs as $log) {
-                $log->setMetaInfo($log->getMetaInfo() + ['a_name' => 'Archived']);
-                $log->setMetaInfo($log->getMetaInfo() + ['c_name' => 'Books']);
-                $log->setMetaInfo($log->getMetaInfo() + ['ip' => $this->request->clientIp()]);
-                $log->setMetaInfo($log->getMetaInfo() + ['url' => Router::url(null, true)]);
-                $log->setMetaInfo($log->getMetaInfo() + ['slug' => $this->Authentication->getIdentity('slug')->getIdentifier('slug')]);
-            }
-        });
+		$this->set('title', 'Books Edit');
+		EventManager::instance()->on('AuditStash.beforeLog', function ($event, array $logs) {
+			foreach ($logs as $log) {
+				$log->setMetaInfo($log->getMetaInfo() + ['a_name' => 'Archived']);
+				$log->setMetaInfo($log->getMetaInfo() + ['c_name' => 'Books']);
+				$log->setMetaInfo($log->getMetaInfo() + ['ip' => $this->request->clientIp()]);
+				$log->setMetaInfo($log->getMetaInfo() + ['url' => Router::url(null, true)]);
+				$log->setMetaInfo($log->getMetaInfo() + ['slug' => $this->Authentication->getIdentity('slug')->getIdentifier('slug')]);
+			}
+		});
         $book = $this->Books->get($id, [
             'contain' => [],
         ]);
         if ($this->request->is(['patch', 'post', 'put'])) {
             $book = $this->Books->patchEntity($book, $this->request->getData());
-            $book->status = 2; //archived
+			$book->status = 2; //archived
             if ($this->Books->save($book)) {
                 $this->Flash->success(__('The book has been archived.'));
 
-                return $this->redirect($this->referer());
+				return $this->redirect($this->referer());
             }
             $this->Flash->error(__('The book could not be archived. Please, try again.'));
         }
